52e45f4659555332276fed1ee85d9d68571f5576
pepe-romeros <pepe.romeros@gmail.com>
2017-10-31 15:42:47 -0400

    Add MainPresenter implement Presenter contract.

---
+++ app/src/main/java/com/example/mvpexample/MainPresenter.java
@@ -0,0 +1,13 @@
+package com.example.mvpexample;
+
+
+public class MainPresenter implements MainContract.Presenter {
+
+    // TODO - Declare model objects and Constructor
+
+    @Override
+    public void generateResult() {
+
+    }
+
+}
